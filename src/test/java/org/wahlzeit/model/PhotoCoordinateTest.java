c9567cd4a2c0e7f094e52d13a351149c92874379
wh473v32 <wh3233v32@gmail.com>
2015-10-26 01:44:13 +0100

    final version hw03

--- src/test/java/org/wahlzeit/model/PhotoCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/PhotoCoordinateTest.java
@@ -1,48 +1,48 @@
+/*
+ * PhotoCoordinateTest
+ * Version 1.0
+ * Date 26.10.2015
+ * Copyright (c) 2015 by Sabrina Jahn
+ */
+
 package org.wahlzeit.model;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PhotoCoordinateTest {
 
-private Photo photo;
-private Photo photo2;
-	
-	@Before
-	public void setUp() throws Exception {
-		photo = new Photo();
-		photo2 = new Photo();
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void test() {
-		//constructor test
-		assertNotNull(photo);
-		assertNotNull(photo2);
-		
-		//coordinate constructor test
-		photo.location = new Coordinate(50, 150);
-		double lat1 = photo.location.getLatitude();
-		double lon1 = photo.location.getLongitude();
+
+		Coordinate location;
+		Coordinate location2;
+		Coordinate location3;
+		// coordinate constructor test
+		location = new Coordinate(50, 150);
+		double lat1 = location.getLatitude();
+		double lon1 = location.getLongitude();
 		assertTrue(lat1 == 50);
 		assertTrue(lon1 == 150);
-		
-		//TODO distance test
-		photo2.location = new Coordinate(-40, -160);
-		Coordinate test = photo.location.getDistance(photo2.location);
+
+		// distance tests
+		location2 = new Coordinate(-40, -160);
+		Coordinate test = location.getDistance(location2);
 		assertTrue(test.getLatitude() == 90);
-		assertTrue(test.getLongitude() == 210);
-		
-		fail("Not yet implemented");
+		assertTrue(test.getLongitude() == -50);
+
+		test = location2.getDistance(location);
+		assertTrue(test.getLatitude() == -90);
+		assertTrue(test.getLongitude() == 50);
+
+		// Input validation test
+		try {
+			location3 = new Coordinate(-91, -181);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 }
